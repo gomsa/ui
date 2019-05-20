a185a046142eb0808b6b6c5741e23323031025c4
bigrocs <bigrocs@qq.com>
2019-05-20 15:56:36 +0800

    自动提交 git 代码

---
+++ bak/src/api/user.js
@@ -0,0 +1,30 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    method: 'post',
+    data: {
+      'service': 'user-api',
+      'method': 'Auth.Auth',
+      'request': data
+    }
+  })
+}
+
+export function getInfo() {
+  return request({
+    method: 'post',
+    data: {
+      'service': 'user-api',
+      'method': 'Users.Info',
+      'request': {}
+    }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/user/logout',
+    method: 'post'
+  })
+}
